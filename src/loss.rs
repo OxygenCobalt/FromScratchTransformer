bf727733816790b671168a5419c23193c8af005a
Alexander Capehart <alex@oxycblt.org>
2025-10-21 13:26:47 -0600

    fix busted loglikelihood function
    regrettably this was ai slop from some really intensive
    llm debugging

--- src/loss.rs
+++ src/loss.rs
@@ -16,12 +16,6 @@ pub struct LogLikelihood;
 
 impl Loss for LogLikelihood {
     fn loss<T: Tensor>(&self, batch_activations: &T, output: &T) -> T {
-        let max = batch_activations.colmax().unwrap();
-        let mut shape = vec![1];
-        shape.extend_from_slice(batch_activations.shape()[1..].into());
-        let max_broadcast = max.clone().reshape(&shape).unwrap();
-        let shifted = batch_activations.clone().sub(&max_broadcast).unwrap();
-        let log_sum_exp = shifted.clone().exp().sum().ln().add(&max).unwrap();
-        log_sum_exp.sub(&batch_activations.at_argmax(&output).unwrap()).unwrap()
+        batch_activations.at_argmax(&output).unwrap().ln().neg()
     }
 }
